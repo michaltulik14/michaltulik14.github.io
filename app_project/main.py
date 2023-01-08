780b2ea7170c376193d2e5d16e2f69c9d7b4cd23
michaltulik14 <tulikmichal3ai@gmail.com>
2023-01-08 22:15:49 +0100

    Push main.py

---
+++ app_project/main.py
@@ -0,0 +1,11 @@
+from tkinter import * 
+
+
+
+root = Tk()
+root.geometry("600x600")            #window dimensions
+root.configure(bg="#cccccc")        #configure window
+root.title("Michał Tulik")          #window title
+root.resizable(False,False)         #don't allow window resizing
+root.iconbitmap("media/icon.ico")   #add icon
+root.mainloop()                     #generate window
