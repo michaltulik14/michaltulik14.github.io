cd809a4b6b5637e60a2e366bd9a7872e9314027f
michaltulik14 <tulikmichal3ai@gmail.com>
2023-01-10 14:30:13 +0100

    10-01-2023

--- app_project/main.py
+++ app_project/main.py
@@ -1,5 +1,6 @@
 from tkinter import * 
-
+from bs4 import BeautifulSoup
+import math
 
 
 root = Tk()
@@ -8,4 +9,48 @@ root.configure(bg="#cccccc")        #configure window
 root.title("Michał Tulik")          #window title
 root.resizable(False,False)         #don't allow window resizing
 root.iconbitmap("media/icon.ico")   #add icon
+
+#test
+
+def button_comand():
+    x=int(entry1.get())
+    y=int(entry2.get())
+    z=int(entry3.get())
+
+    delta = y**2 - (4*(x*z))
+    print(delta)
+    if delta<0:
+        print("mniejsza od zera")
+    elif delta==0:
+        print("rowna zero")
+        result = - (y/(2*x))
+        result2 = "Miejsce x0 to ",result
+        siema.config(text=result2) 
+    else:
+        x0 = (-y - math.sqrt(delta))/(2*x)
+        x1 = (-y + math.sqrt(delta))/(2*x)
+        print(x0," ",x1)
+
+        result = "Miejsce x0 wynosi ",x0," a miejsca x1 to",x1
+
+        siema.config(text=result)     
+
+        
+
+entry1 = Entry(root, width=20)
+entry1.pack()
+
+entry2 = Entry(root, width=20)
+entry2.pack()
+
+entry3 = Entry(root, width=20)
+entry3.pack()
+
+
+Button(root, text="Policz miejsca zerowe", command=button_comand).pack()
+
+siema = Label(root, text="wynik")
+siema.pack()
+
+
 root.mainloop()                     #generate window
