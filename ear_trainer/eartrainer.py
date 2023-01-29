1a0f222b195c6e4b38289ebd188267221e7a4248
michaltulik14 <tulikmichal3ai@gmail.com>
2023-01-29 21:59:58 +0100

    EarTrainer update 29-01-2023 22-00

--- ear_trainer/eartrainer.py
+++ ear_trainer/eartrainer.py
@@ -17,12 +17,10 @@ from PyQt6.QtGui import QIcon
 import sys
 
 all_notes = ["C","D","E","F","G","A","B","C#","D#","F#","G#","A#"]
-all_chords = ["cmaj","cmin"]
+all_chords = ["cmaj","cmin","dom7","min7","maj7"]
+all_interv = ["2m","2w","3m","3w","trit","4th","5th","6m","6w","7m","7w"]
 sound_path = "/Users/michal/Documents/portfolio/michaltulik14.github.io/ear_trainer/"
 format = ".mp3"
-
-
-
 
 class Window(QWidget):
     def __init__(self):
@@ -32,6 +30,8 @@ class Window(QWidget):
      
         self.setWindowTitle("EarTrainer")
         self.note = ""
+        self.interv = ""
+        self.chord = ""
 
         layout = QGridLayout()
         self.setLayout(layout)    
@@ -56,12 +56,12 @@ class Window(QWidget):
         tab_chord = QTabWidget()
         chords = QWidget(self)
         layout5 = QGridLayout()
-
+        layout5.setVerticalSpacing(50)
         chords.setLayout(layout5)
         tab.addTab(chords,"Chords")         #Add Tab to page
 
 
-        layout.addWidget(tab_chord,1,0)
+        #layout.addWidget(tab_chord,1,0)
 
 #Create Intervals TAB
 
@@ -72,27 +72,23 @@ class Window(QWidget):
         interval.setLayout(layout8)
         tab.addTab(interval,"Intevals")         #Add Tab to page
 
-        layout.addWidget(tab_interval,2,0)
+        #layout.addWidget(tab_interval,2,0)
 
 # CREATING BUTTONS AND LABELS FOR PERFECT PITCH (TAB 1)           
 
         button = QPushButton("Start practice")
         button.setFixedWidth(100)
-
         layout1.addWidget(button,0,2,Qt.AlignmentFlag.AlignCenter)
-        
         button.clicked.connect(self.display)
 
         again = QPushButton("Play again")
         again.setFixedWidth(100)
         layout1.addWidget(again,0,4,Qt.AlignmentFlag.AlignCenter)
-
         again.clicked.connect(self.again)
 
         c_note = QPushButton("C")
         c_note.setFixedWidth(100)
         layout1.addWidget(c_note, 1,0,Qt.AlignmentFlag.AlignLeft)
-        
         c_note.clicked.connect(self.verify_c)
 
         d_note = QPushButton("D")
@@ -104,19 +100,16 @@ class Window(QWidget):
         e_note = QPushButton("E")
         e_note.setFixedWidth(100)
         layout1.addWidget(e_note, 1,2,Qt.AlignmentFlag.AlignLeft)
-  
         e_note.clicked.connect(self.verify_e)
 
         f_note = QPushButton("F")
         f_note.setFixedWidth(100)
         layout1.addWidget(f_note, 1,3,Qt.AlignmentFlag.AlignLeft)
-   
         f_note.clicked.connect(self.verify_f)
 
         g_note = QPushButton("G")
         g_note.setFixedWidth(100)
         layout1.addWidget(g_note, 1,4,Qt.AlignmentFlag.AlignLeft)
-
         g_note.clicked.connect(self.verify_g)
 
         a_note = QPushButton("A")
@@ -140,13 +133,11 @@ class Window(QWidget):
         dsharp_note = QPushButton("D#")
         dsharp_note.setFixedWidth(100)
         layout1.addWidget(dsharp_note, 2,3,Qt.AlignmentFlag.AlignLeft)
-       
         dsharp_note.clicked.connect(self.verify_dsharp)
         
         fsharp_note = QPushButton("F#")
         fsharp_note.setFixedWidth(100)
         layout1.addWidget(fsharp_note, 2,4,Qt.AlignmentFlag.AlignLeft)
-        
         fsharp_note.clicked.connect(self.verify_fsharp)
 
         gsharp_note = QPushButton("G#")
@@ -157,21 +148,132 @@ class Window(QWidget):
         asharp_note = QPushButton("A#")
         asharp_note.setFixedWidth(100)
         layout1.addWidget(asharp_note, 3,1,Qt.AlignmentFlag.AlignLeft)
-        
         asharp_note.clicked.connect(self.verify_asharp)
 
-# OUTPUT RESULT
+# OUTPUT RESULT FOR PERFECT PITCH 
         self.result_label=QLabel("")
         self.result_label.setFixedWidth(100)
-        self.result_label.setFont(QFont("Trebuchet MS",30))
-       
+        self.result_label.setFont(QFont("Trebuchet MS",30)) 
         layout1.addWidget(self.result_label, 4,2,Qt.AlignmentFlag.AlignLeft)
-       
+#OUTPUT RESULT FOR CHORDS
+        self.chord_label=QLabel("")
+        self.chord_label.setFixedWidth(100)
+        self.chord_label.setFont(QFont("Trebuchet MS",30)) 
+        layout5.addWidget(self.chord_label, 4,2,Qt.AlignmentFlag.AlignLeft)
 
+#OUTPUT RESULT FOR INTERVALS
+        self.interv_label=QLabel("")
+        self.interv_label.setFixedWidth(100)
+        self.interv_label.setFont(QFont("Trebuchet MS",30)) 
+        layout8.addWidget(self.interv_label, 4,2,Qt.AlignmentFlag.AlignLeft)
+       
+#CHORD BUTTONS
+        chord_button = QPushButton("Start practice")
+        chord_button.setFixedWidth(100)
+        layout5.addWidget(chord_button,0,2,Qt.AlignmentFlag.AlignCenter)
+        chord_button.clicked.connect(self.display_chord)
+
+        chord_again = QPushButton("Play again")
+        chord_again.setFixedWidth(100)
+        layout5.addWidget(chord_again,0,4,Qt.AlignmentFlag.AlignCenter)
+        chord_again.clicked.connect(self.again_chord)
   
+        maj_chord = QPushButton("Maj")
+        maj_chord.setFixedWidth(100)
+        layout5.addWidget(maj_chord, 1,0,Qt.AlignmentFlag.AlignLeft)
+        maj_chord.clicked.connect(self.verify_majchord)
+
+        min_chord = QPushButton("Min")
+        min_chord.setFixedWidth(100)
+        layout5.addWidget(min_chord, 1,1,Qt.AlignmentFlag.AlignLeft)
+        min_chord.clicked.connect(self.verify_minchord)
+
+        dom7_chord = QPushButton("Dom7")
+        dom7_chord.setFixedWidth(100)
+        layout5.addWidget(dom7_chord, 1,2,Qt.AlignmentFlag.AlignLeft)
+        dom7_chord.clicked.connect(self.verify_domchord)
+
+        maj7_chord = QPushButton("Maj7")
+        maj7_chord.setFixedWidth(100)
+        layout5.addWidget(maj7_chord, 1,3,Qt.AlignmentFlag.AlignLeft)
+        maj7_chord.clicked.connect(self.verify_maj7chord)
+
+        min7_chord = QPushButton("Min7")
+        min7_chord.setFixedWidth(100)
+        layout5.addWidget(min7_chord, 1,4,Qt.AlignmentFlag.AlignLeft)
+        min7_chord.clicked.connect(self.verify_min7chord)
+
+
+#INTERVALS BUTTONS
+        interv_button = QPushButton("Start practice")
+        interv_button.setFixedWidth(100)
+        layout8.addWidget(interv_button,0,2,Qt.AlignmentFlag.AlignCenter)
+        interv_button.clicked.connect(self.display_interv)
+
+        interv_again = QPushButton("Play again")
+        interv_again.setFixedWidth(100)
+        layout8.addWidget(interv_again,0,4,Qt.AlignmentFlag.AlignCenter)
+        interv_again.clicked.connect(self.again_interv)
+
+        m2_interv = QPushButton("m2")
+        m2_interv.setFixedWidth(100)
+        layout8.addWidget(m2_interv, 1,0,Qt.AlignmentFlag.AlignLeft)
+        m2_interv.clicked.connect(self.verify_m2interv)
+
+        w2_interv = QPushButton("M2")
+        w2_interv.setFixedWidth(100)
+        layout8.addWidget(w2_interv, 1,1,Qt.AlignmentFlag.AlignLeft)
+        w2_interv.clicked.connect(self.verify_w2interv)
+
+        m3_interv = QPushButton("m3")
+        m3_interv.setFixedWidth(100)
+        layout8.addWidget(m3_interv, 1,2,Qt.AlignmentFlag.AlignLeft)
+        m3_interv.clicked.connect(self.verify_m3interv)
+
+        w3_interv = QPushButton("M3")
+        w3_interv.setFixedWidth(100)
+        layout8.addWidget(w3_interv, 1,3,Qt.AlignmentFlag.AlignLeft)
+        w3_interv.clicked.connect(self.verify_w3interv)
+
+        kwart_interv = QPushButton("4th")
+        kwart_interv.setFixedWidth(100)
+        layout8.addWidget(kwart_interv, 1,4,Qt.AlignmentFlag.AlignLeft)
+        kwart_interv.clicked.connect(self.verify_kwart)
+
+        trit_interv = QPushButton("4+")
+        trit_interv.setFixedWidth(100)
+        layout8.addWidget(trit_interv, 2,0,Qt.AlignmentFlag.AlignLeft)
+        trit_interv.clicked.connect(self.verify_trit)
+
+        fifth_interv = QPushButton("5th")
+        fifth_interv.setFixedWidth(100)
+        layout8.addWidget(fifth_interv, 2,1,Qt.AlignmentFlag.AlignLeft)
+        fifth_interv.clicked.connect(self.verify_fifth)
+
+        m6_interv = QPushButton("m6")
+        m6_interv.setFixedWidth(100)
+        layout8.addWidget(m6_interv, 2,2,Qt.AlignmentFlag.AlignLeft)
+        m6_interv.clicked.connect(self.verify_m6interv)
+
+        w6_interv = QPushButton("M6")
+        w6_interv.setFixedWidth(100)
+        layout8.addWidget(w6_interv, 2,3,Qt.AlignmentFlag.AlignLeft)
+        w6_interv.clicked.connect(self.verify_w6interv)
+
+        m7_interv = QPushButton("m7")
+        m7_interv.setFixedWidth(100)
+        layout8.addWidget(m7_interv, 2,4,Qt.AlignmentFlag.AlignLeft)
+        m7_interv.clicked.connect(self.verify_m7interv)
+
+        w7_interv = QPushButton("M7")
+        w7_interv.setFixedWidth(100)
+        layout8.addWidget(w7_interv, 3,2,Qt.AlignmentFlag.AlignLeft)
+        w7_interv.clicked.connect(self.verify_w7interv)
 
         
-
+        
+        
+# PLAYING SINGLE NOTES 
     def display(self):
         
         #self.note = "D"
@@ -181,14 +283,15 @@ class Window(QWidget):
         play_note = AudioSegment.from_mp3(self.choice)
         play(play_note)
         print(self.note)
-    
+
+# REPEAT SINGLE NOTES 
     def again(self):
         #play note once again
 
         play_note = AudioSegment.from_mp3(self.choice)
         play(play_note)
         
-        
+# SINGLE NOTES VERIFICATION
     def verify_c(self):
         if self.note == "C":
             print("success")
@@ -198,9 +301,7 @@ class Window(QWidget):
             print("chujowo")
             self.result_label.setText("Wrong")
             self.result_label.setStyleSheet("color:red;")
-        QTimer.singleShot(2000,self.display)
-        
-        
+        QTimer.singleShot(2000,self.display) 
 
     def verify_d(self):
         if self.note == "D":
@@ -313,26 +414,214 @@ class Window(QWidget):
             self.result_label.setStyleSheet("color:red;")
         QTimer.singleShot(2000,self.display)
 
+
+#PLAY SINGLE CHORD
     def display_chord(self):
         
         #self.note = "D"
         self.chord = random.choice(all_chords)
-        self.result_label.setText("")
-        self.choice = sound_path+self.note+format
+        self.chord_label.setText("")
+        self.choice = sound_path+self.chord+format
         play_note = AudioSegment.from_mp3(self.choice)
         play(play_note)
-        print(self.note)
+        print(self.chord)
     
+# REPEAT CHORD
     def again_chord(self):
-        #play note once again
-
         play_note = AudioSegment.from_mp3(self.choice)
         play(play_note)
+    
+#CHORDS VERIFICATION
+
+    def verify_majchord(self):
+        if self.chord == "cmaj":
+            print("success")
+            self.chord_label.setText("Correct")
+            self.chord_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.chord_label.setText("Wrong")
+            self.chord_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_chord)
+
+    def verify_minchord(self):
+        if self.chord == "cmin":
+            print("success")
+            self.chord_label.setText("Correct")
+            self.chord_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.chord_label.setText("Wrong")
+            self.chord_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_chord)
+    def verify_domchord(self):
+        if self.chord == "dom7":
+            print("success")
+            self.chord_label.setText("Correct")
+            self.chord_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.chord_label.setText("Wrong")
+            self.chord_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_chord)
+    def verify_min7chord(self):
+        if self.chord == "min7":
+            print("success")
+            self.chord_label.setText("Correct")
+            self.chord_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.chord_label.setText("Wrong")
+            self.chord_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_chord)
+    def verify_maj7chord(self):
+        if self.chord == "maj7":
+            print("success")
+            self.chord_label.setText("Correct")
+            self.chord_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.chord_label.setText("Wrong")
+            self.chord_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_chord)
+
+
+#PLAY SINGLE INTERV
+    def display_interv(self):
         
+        #self.note = "D"
+        self.interv = random.choice(all_interv)
+        self.interv_label.setText("")
+        self.choice = sound_path+self.interv+format
+        play_note = AudioSegment.from_mp3(self.choice)
+        play(play_note)
+        print(self.interv)
+    
+# REPEAT INTERV
+    def again_interv(self):
+        play_note = AudioSegment.from_mp3(self.choice)
+        play(play_note)
 
 
+# INTERVALS VERIFICATION : 
+    def verify_m2interv(self):
+        if self.interv == "2m":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
 
+    def verify_w2interv(self):
+        if self.interv == "2w":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+    def verify_m3interv(self):
+        if self.interv == "3m":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+    def verify_w3interv(self):
+        if self.interv == "3w":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+    
+    def verify_kwart(self):
+        if self.interv == "4th":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
 
+    def verify_trit(self):
+        if self.interv == "trit":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+
+    def verify_fifth(self):
+        if self.interv == "5th":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+
+    def verify_m6interv(self):
+        if self.interv == "6m":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+    
+    def verify_w6interv(self):
+        if self.interv == "6w":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+
+    def verify_m7interv(self):
+        if self.interv == "7w":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
+
+    def verify_w7interv(self):
+        if self.interv == "7w":
+            print("success")
+            self.interv_label.setText("Correct")
+            self.interv_label.setStyleSheet("color:green;")
+        else:
+            print("chujowo")
+            self.interv_label.setText("Wrong")
+            self.interv_label.setStyleSheet("color:red;")
+        QTimer.singleShot(2000,self.display_interv)
 
 app = QApplication(sys.argv)
 
